eaedf38906c3369ea3657731cef35fce302a6d04
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-08-02 23:16:29 -0300

    routes to get a recipe by his id

--- src/routes/recipes.routes.ts
+++ src/routes/recipes.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import routerHelper from "../helpers/routerHelper";
 import newRecipe from "../controllers/recipes/newRecipe.controller";
+import getRecipe from "../controllers/recipes/getRecipe.controller";
 
 const router = Router();
 
 router.post("/create", routerHelper(newRecipe));
+router.get("/recipe/:id", routerHelper(getRecipe));
 
 export default router;
